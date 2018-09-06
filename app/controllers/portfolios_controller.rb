3de89827c0fe478fe20f6fc9023d7abbcf82b29d
Philip Baumann <philip.baumann@gmail.com>
2018-09-06 16:13:20 +0200

    Implemented show functionality for portfolio items, added links to show and edit items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -23,7 +23,19 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Updated portfolio item' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   def show
-    # @portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
