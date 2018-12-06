9f2aa726daa8a95ddc9a03069c557a371b33380d
Philip Baumann <philip.baumann@coupa.com>
2018-12-06 15:27:38 +0100

    Implemented view helper for authentication links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      ('<li>' + (link_to 'Logout', destroy_user_session_path, method: :delete) + '</li>').html_safe
+    else
+      ('<li>' + (link_to 'Login', new_user_session_path) + '</li>' \
+      '<li>' + (link_to 'Register', new_user_registration_path) + '</li>').html_safe
+    end
+  end
 end
