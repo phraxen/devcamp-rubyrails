19bd51a2e1b469ba46d77a9a80cbb60c5c26762d
Philip Baumann <philip.baumann@coupa.com>
2018-12-11 16:02:10 +0100

    Implemented authorization engine for portfolios and blogs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User)
-      content_tag(:li, (link_to 'Logout', destroy_user_session_path, method: :delete), class: 'nav-item')
-    else
+    if current_user.is_a?(GuestUser)
       content_tag(:li, (link_to 'Login', new_user_session_path), class: 'nav-item') +
         content_tag(:li, (link_to 'Register', new_user_registration_path), class: 'nav-item')
+    else
+      content_tag(:li, (link_to 'Logout', destroy_user_session_path, method: :delete), class: 'nav-item')
     end
   end
 
