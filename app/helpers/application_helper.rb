de3f39fbf7f7cbced8f72a96c3cb1392fcf4b823
Philip Baumann <philip.baumann@coupa.com>
2018-12-11 16:30:04 +0100

    Installed styles for application layout. Base implementation of the navigation menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def login_helper
     if current_user.is_a?(GuestUser)
-      content_tag(:li, (link_to 'Login', new_user_session_path), class: 'nav-item') +
-        content_tag(:li, (link_to 'Register', new_user_registration_path), class: 'nav-item')
+      (link_to 'Login', new_user_session_path, :class => 'nav-link') +
+        (link_to 'Register', new_user_registration_path, :class => 'nav-link')
     else
       content_tag(:li, (link_to 'Logout', destroy_user_session_path, method: :delete), class: 'nav-item')
     end
